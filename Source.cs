2d685d885cfef3ef77f7c78f5d04127e7b0e8845
CriticalShow <65967000+FooqX@users.noreply.github.com>
2022-01-29 08:48:07 -0800

    Bug fixes and performance improvements

--- Source.cs
+++ Source.cs
@@ -37,8 +37,11 @@ if (File.Exists(webhookPath)) {
     Console.Write("Clean webhooks (Y)es/(N)o >> ");
 
     if ("y".Equals(Console.ReadLine().ToLower())) {
-        File.WriteAllText(webhookPath, "");
         Console.WriteLine("- webhooks.txt " + new FileInfo(webhookPath).Length + " KB ~> 0 KB");
+        FileStream fs = File.OpenWrite(webhookPath);
+        fs.SetLength(0);
+        fs.Dispose();
+        fs.Close();
     }
 } else {
     File.Create(webhookPath);
@@ -77,15 +80,38 @@ string RandomNumber(int n = 18) {
 Console.WriteLine("\nPreparing...");
 Console.WriteLine("Note: Please be patient, because this can take a while depending on webhook amount");
 Console.WriteLine("\nWriting unbuffered webhooks...");
-using (StreamWriter sw = File.AppendText(path: webhookPath)) {
+try {
+    // Opening streamwriter
+    using StreamWriter sw = File.AppendText(path: webhookPath);
+
+    // Configuring streamwriter for performance
     sw.AutoFlush = false;
     sw.NewLine = "\n";
-    for (uint i = 0; i <= webhookCount; i++) {
+    
+    // Generating webhooks (unbuffered)
+    for (uint i = 0; i < webhookCount; i++) {
         sw.WriteLine($"https://discord.com/api/webhooks/{RandomNumber()}/{RandomToken()}");
     }
+
+    // Buffering
     Console.WriteLine("Done writing!\nBuffering...");
-    sw.Flush();
-    Console.WriteLine("Done buffering!\n");
+    sw.Flush();  // Flushing the stream so it writes changes
+    Console.WriteLine("Done buffering!");
+
+    // Closing streamwriter
+    Console.WriteLine("Disposing StreamWriter...");
+    sw.Dispose();  // Releasing all resources used up by streamwriter
+    Console.WriteLine("Closing StreamWriter...");
+    sw.Close();  // Closing streamwriter
+    Console.WriteLine("Done closing & disposing StreamWriter!\n");
+} catch (Exception e) {
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Error. " + e.Message);
+    Console.ResetColor();
+
+    Console.Write("Press any key to continue...");
+    Console.ReadKey();
+    Environment.Exit(0);
 }
 
 Console.ForegroundColor = ConsoleColor.Green;
